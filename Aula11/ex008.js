3bc725c66efd533669f7be9d4528d15bf6e6a278
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-11-25 16:48:47 -0300

    create ex 08js

---
+++ Aula11/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 45
+if (velocidade > 80) {
+    console.log(`A velocidade do seu carro é de ${velocidade}Km/h. Você foi MULTADO!`)
+}
+if(velocidade < 80){
+    console.log(`A sua velocidade foi de ${velocidade}Km/h.`)
+}
+
+console.log(`Faça uma boa viagem!`)
