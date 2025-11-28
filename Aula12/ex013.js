cf82191229884bd00e6c9fd30e28101b7b8b338d
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-11-28 17:44:24 -0300

    condições aninhadas

---
+++ Aula12/ex013.js
@@ -0,0 +1,11 @@
+var agora = new Date()
+var hora = agora.getHours()
+if (hora < 12){
+    console.log(`Bom dia`)
+} else if(hora < 18){
+    console.log(`Boa tarde!`)
+} else if (hora < 24){
+    console.log(`Boa noite!`)
+}
+
+console.log(`São exatamente ${hora} horas!`)
