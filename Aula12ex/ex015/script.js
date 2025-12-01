7b8229d1b38739d1c2eb98a1d9e129f0656adace
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-01 10:08:22 -0300

    Exercícios JavaScript (Parte 2)

---
+++ Aula12ex/ex015/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    var hora = 02
+    if (hora < 12 && hora > 0){
+        img.src = 'fotomanha.png'
+        msg.innerHTML = `Agora são ${hora} horas. BOM DIA!`
+        document.body.style.background = '#dad74fff'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'fototarde.png'
+        msg.innerHTML = `Agora são ${hora} horas. BOA TARDE!`
+        document.body.style.background = '#dbcd51ff'
+    } else {
+        img.src = 'fotonoite.png'
+        msg.innerHTML = `Agora são ${hora} horas. BOA NOITE`
+        document.body.style.background = '#2b5e6c'
+    }
+
+}
