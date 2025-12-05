93e13a1af5f85ef42915203e34cbdeb97c94f4e1
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-05 10:39:58 -0300

    criando variaveis

---
+++ Aula12ex/pratica/code.js
@@ -0,0 +1,9 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nascimento = window.document.querySelector('input#txtano')
+    var resultado = window.document.querySelector('div#resultado')
+    if(nascimento.value.length == 0 || Number(nascimento.value) > ano){
+        window.alert('Erro! Dados invalidos, tente novamente!')
+    }
+}
