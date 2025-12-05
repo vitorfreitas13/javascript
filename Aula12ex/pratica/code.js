187d657cdf43838eb75c60160501a3b276d47a88
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-05 12:38:25 -0300

    adicionando javascript

--- Aula12ex/pratica/code.js
+++ Aula12ex/pratica/code.js
@@ -5,5 +5,7 @@ function verificar(){
     var resultado = window.document.querySelector('div#resultado')
     if(nascimento.value.length == 0 || Number(nascimento.value) > ano){
         window.alert('Erro! Dados invalidos, tente novamente!')
+    } else {
+        var sexo = document.getElementsByName('radsex')
     }
 }
