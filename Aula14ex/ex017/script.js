771840fea34f9eb8c47c34a5280324c47e8402b3
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-12 18:00:47 -0300

    finalizando exeercicios

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,51 +1,14 @@
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = window.document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('ERRO! Verifique os dados e tente novamente!')
-    } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+    var numero = document.querySelector('#numero');
+    var resultado = document.querySelector('.res');
+    var contador = 1
+    var num = Number(numero.value);
+    
 
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if(idade >= 0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'menino.png')
-            } else if (idade < 21){
-                //jovem
-                img.setAttribute('src', 'homemjovem')
-            } else if (idade < 50){
-                //adulto
-                img.setAttribute('src', 'homemadulto.png')
-            }else {
-                //idoso
-                img.setAttribute('src', 'idoso.png')
-            }
-        } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if(idade >= 0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'menina.png')
-            } else if (idade < 21){
-                //jovem
-                img.setAttribute('src', 'mulherjovem.png')
-            } else if (idade < 50){
-                //adulto
-                img.setAttribute('src', 'mulheradulta.png')
-            }else {
-                //idoso
-                img.setAttribute('src', 'idosa.png')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
-        res.appendChild(img)
-        
+    resultado.innerHTML = ''; // limpa antes de mostrar
+
+    for (var contador = 1; contador <= 10; contador++) {
+        var soma = num * contador
+        resultado.innerHTML += `${contador} x ${num} = ${soma}  <br> `;
     }
 }
