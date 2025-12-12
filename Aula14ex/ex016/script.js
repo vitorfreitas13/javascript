246af5d6daaeb919b15ce4d51f1e0f01f6c1567e
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-12 16:53:00 -0300

    Exercícios JavaScript (Parte 4)

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,51 +1,44 @@
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = window.document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('ERRO! Verifique os dados e tente novamente!')
-    } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+    var inicioInput = document.querySelector('#inicio');
+    var fimInput = document.querySelector('#fim');
+    var passoInput = document.querySelector('#passo');
+    var res = document.querySelector('.resultado'); // Referência à área de resultado
 
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if(idade >= 0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'menino.png')
-            } else if (idade < 21){
-                //jovem
-                img.setAttribute('src', 'homemjovem')
-            } else if (idade < 50){
-                //adulto
-                img.setAttribute('src', 'homemadulto.png')
-            }else {
-                //idoso
-                img.setAttribute('src', 'idoso.png')
-            }
-        } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if(idade >= 0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'menina.png')
-            } else if (idade < 21){
-                //jovem
-                img.setAttribute('src', 'mulherjovem.png')
-            } else if (idade < 50){
-                //adulto
-                img.setAttribute('src', 'mulheradulta.png')
-            }else {
-                //idoso
-                img.setAttribute('src', 'idosa.png')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
-        res.appendChild(img)
-        
+    // 1. Obter os Valores e convertê-los para número
+    var i = Number(inicioInput.value);
+    var f = Number(fimInput.value);
+    var p = Number(passoInput.value);
+
+    // 2. Validação
+    if (inicioInput.value.length === 0 || fimInput.value.length === 0 || passoInput.value.length === 0) {
+        // Verifica se algum campo está vazio
+        res.innerHTML = 'Impossível contar! Verifique os dados.';
+        return; // Sai da função
     }
+
+    if (p <= 0) {
+        // Garante que o passo não é zero ou negativo
+        window.alert('Passo inválido! Considerando PASSO 1.');
+        p = 1;
+    }
+
+    // Limpa o resultado anterior e começa a contagem
+    res.innerHTML = 'Contando: <br>';
+    var resultadoContagem = '';
+    
+    // 3. Realizar o Loop e a Contagem
+    if (i < f) {
+        // Contagem Crescente
+        for (let c = i; c <= f; c += p) {
+            resultadoContagem += `${c} 👉 `;
+        }
+    } else {
+        // Contagem Regressiva
+        for (let c = i; c >= f; c -= p) {
+            resultadoContagem += `${c} 👉 `;
+        }
+    }
+
+    // 4. Exibir o Resultado
+    res.innerHTML += resultadoContagem + '🏁'; 
 }
