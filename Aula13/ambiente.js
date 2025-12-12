63bb6a71e03eb122fc4e419ba5cf8c0985f75bc1
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-12 09:29:39 -0300

    Aula 13 – Repetições (Parte 1)

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,5 +1,13 @@
+/*
 var c = 1
-while (c <= 6){
-    console.log('tudo bem?')
+while (c <= 10){
+    console.log(`Passo ${c}`)
     c++
 }
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 10)
