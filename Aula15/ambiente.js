64d383a2225f2d85bfae05b1abe736227f1055db
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-23 12:26:05 -0300

    praticando com vetores

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -6,3 +6,11 @@ num.push(1)
 console.log(num)
 console.log(`O vetor tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é, ${num[0]}.`)
+
+let pos = num.indexOf(8)
+if(pos == -1){
+    console.log('O numero não foi encontrado!')
+} else {
+    console.log(`A posição é ${pos} `)
+}
+
---
+++ Aula15/vetornatela.js
@@ -0,0 +1,25 @@
+let valores = [5, 2, 9, 4, 7, 6, 10]
+
+//console.log(valores)
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+console.log(valores[6])
+console.log(valores[7])
+*/
+
+/*
+for(let pos=0; pos < valores.length; pos++){
+    console.log()
+}
+
+
+for(let pos in valores) {
+    console.log(`O valor da posição ${pos}  é igual a ${valores[pos]}.`)
+}
+*/
+
