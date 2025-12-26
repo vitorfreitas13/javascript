4b5f130a4361fd1cd0fbf593e49a1da0d1abe0f3
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2025-12-26 18:45:38 -0300

    Praticando js

---
+++ teste/script.js
@@ -0,0 +1,10 @@
+function parOuImpar(num, num2){
+    if(num % 2 === 0){
+        console.log(`${num} é par!`)
+    } else {
+        console.log(`${num} é ímpar!`)
+    } 
+}
+
+parOuImpar(10)
+parOuImpar(13)
